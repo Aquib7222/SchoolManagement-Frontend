4a3daca819858be5835814b0a0c2bed766ef4eae
Aquib7222 <aquibshahzada@gmail.com>
2026-09-08 22:08:48 +0530

    update backend live url in axios instance

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -30,18 +30,50 @@
 
 // export default axiosInstance;
 
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: `http://${window.location.hostname}:8080`,
+// });
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401 || error.response?.status === 403) {
+//       localStorage.clear();
+//       window.location.href = "/login";
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axiosInstance;
+
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `http://${window.location.hostname}:8080`,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => Promise.reject(error)
@@ -54,6 +86,7 @@ axiosInstance.interceptors.response.use(
       localStorage.clear();
       window.location.href = "/login";
     }
+
     return Promise.reject(error);
   }
 );
